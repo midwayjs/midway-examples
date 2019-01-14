9749fb31b439748c47780caf1e41bb3a7f6b67c9
Harry Chen <czy88840616@gmail.com>
2019-01-14 15:46:30 +0800

    chore: fix ci

--- demo-extended-koa/test/app/controller/home.test.ts
+++ demo-extended-koa/test/app/controller/home.test.ts
@@ -1,21 +1,11 @@
-/* tslint:disable */
 const { app, assert } = require('midway-mock/bootstrap');
-/* tslint:enable */
-
-declare var describe;
-declare var it;
 
 describe('test/app/controller/home.test.ts', () => {
-
-  it('should assert', function* () {
-    // const ctx = app.mockContext({});
-    // yield ctx.service.xx();
-  });
 
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, midway')
+      .expect('harry welcome')
       .expect(200);
   });
 });
