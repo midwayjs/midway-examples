73bc554b03e9f9ede453a4c70034f2a382fb9b45
Harry Chen <czy88840616@gmail.com>
2019-01-16 10:36:45 +0800

    chore: remove code

--- demo-view-nunjucks/test/controller/home.test.ts
+++ demo-view-nunjucks/test/controller/home.test.ts
@@ -1,16 +1,12 @@
-import { app, assert } from 'midway-mock/bootstrap';
+import { app } from 'midway-mock/bootstrap';
 
 describe('test/controller/home.test.ts', () => {
-  it('should assert config.keys', () => {
-    const pkg = require('../../package.json');
-    assert(app.config.keys.startsWith(pkg.name));
-  });
 
   it('should GET /', () => {
     return app
       .httpRequest()
       .get('/')
-      .expect('Welcome to midwayjs!')
+      .expect(/This is a midway-nunjucks example/)
       .expect(200);
   });
 });
