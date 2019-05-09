d57ec7d2a0f4068ce3b7866027c4b59623a0235f
Tw93 <tw93@qq.com>
2019-05-09 10:21:23 +0800

    fix(change assert portpath):

--- demo-ant-design-pro/server/src/config/config.local.ts
+++ demo-ant-design-pro/server/src/config/config.local.ts
@@ -7,14 +7,14 @@ module.exports = (appInfo: any) => {
     publicPath: 'public',
     devServer: {
       autoPort: true,
-      command: 'cross-env umi dev --port={port}',
+      command: 'umi dev --port={port}',
+      debug: true,
+      portPath: path.join(appInfo.baseDir, '../run/assetsPort'),
       env: {
         APP_ROOT: path.join(__dirname, '../../../client'),
-        portPath: path.join(appInfo.appDir, 'run/assetsPort'),
         BROWSER: 'none',
         SOCKET_SERVER: 'http://127.0.0.1:{port}'
-      },
-      debug: true
+      }
     }
   };
 
