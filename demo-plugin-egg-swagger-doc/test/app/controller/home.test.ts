ae10ca2c3de46574cdae255c14bf619833c60830
Harry Chen <czy88840616@gmail.com>
2019-08-10 19:37:25 +0800

    chore: update

--- demo-plugin-egg-swagger-doc/test/app/controller/home.test.ts
+++ demo-plugin-egg-swagger-doc/test/app/controller/home.test.ts
@@ -5,11 +5,7 @@ describe('test/app/controller/home.test.ts', () => {
 
   it('should GET /api use router middleware', () => {
     return app.httpRequest()
-      .get('/token')
-      .expect(200)
-      .expect((res) => {
-        console.log(res.text);
-        assert(res.text.length > 20)
-      });
+      .get('/swagger-doc')
+      .expect(200);
   });
 });
