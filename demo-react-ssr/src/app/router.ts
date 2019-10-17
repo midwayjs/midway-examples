2b743bad80fb6a10298e2cb7c0bc892a7926dd3e
文睿 <1342985759@qq.com>
2019-10-17 19:48:11 +0800

    feat：加入midway结合react ssr用例

---
+++ demo-react-ssr/src/app/router.ts
@@ -0,0 +1,12 @@
+const config = require('../../config/config.ssr')
+
+module.exports = function (app: any) {
+  const { router } = app
+  console.log('\x1B[32m 前端路由已经自动绑定，规则如下： \x1B[39m')
+
+  config.routes.map((route: any) => {
+    console.log(`\x1B[32m ${route.path}=>${route.controller}.${route.handler} \x1B[39m `)
+    router.get(`${route.path}`, app.generateController(`${route.controller}.${route.handler}`))
+  })
+
+}
